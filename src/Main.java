1660409c5d59b477fd14da66662cede87510c5d9
Daniel <daniel.merino@iesjulianmarias.es>
2024-10-11 17:23:17 +0200

    V3

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("asdasd");
         System.out.println("Hola2");
+        System.out.println("Hola3");
     }
 }
 
